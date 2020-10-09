6880aaa9e3ade21b1b55c809990508e3204c34f7
Arley Duarte <arleymauricio@gmail.com>
2020-10-09 17:34:14 -0300

    bdd

--- src/App.test.js
+++ src/App.test.js
@@ -2,24 +2,45 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
-
-it('renders correctly', () => {
-    expect(app).toMatchSnapshot();
-});
-
-it('initializas the `state` with a empty lis of gifs', () => {
-  expect(app.state().gifts).toEqual([]);
-});
 
 
-it('adds a new gift to `state` when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click');
-  expect(app.state().gifts).toEqual([{id: 1}]);
-});
 
-it('add a new gift tho the rendered lis whe clicking the `add gif', () => {
 
-  app.find('.btn-add').simulate('click');
-  expect(app.find('.gift-list').children().length).toEqual(2);
+describe('App',  () => {
+  const app = shallow(<App />);
+
+  it('renders correctly', () => {
+      expect(app).toMatchSnapshot();
+  });
+
+
+
+
+  
+  it('initializas the `state` with a empty lis of gifs', () => {
+    expect(app.state().gifts).toEqual([]);
+  });
+  
+
+  describe('when click `add gif` button', () => {
+
+    beforeEach(() => {
+      app.find('.btn-add').simulate('click');
+    });
+
+
+    afterEach(() => {
+      app.setState({gifts: []});
+    });
+    it('adds a new gift to `state`', () => {
+
+      expect(app.state().gifts).toEqual([{id: 1}]);
+    });
+    
+    it('add a new gift tho the rendered list', () => {
+      expect(app.find('.gift-list').children().length).toEqual(2);
+    });
+  });
+  
+
 });
